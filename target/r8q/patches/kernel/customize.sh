7bb6f4e1a603493301b8397bbaf44148859730be
Samuel Pascua <pascua.samuel.14@gmail.com>
2026-02-25 07:44:15 +0800

    r8q: Fetch latest kernel from pascua28/kernel_samsung_r8q
    Signed-off-by: Samuel Pascua <pascua.samuel.14@gmail.com>

--- target/r8q/patches/kernel/customize.sh
+++ target/r8q/patches/kernel/customize.sh
@@ -1,4 +1,8 @@
-LOG "- Replacing kernel"
+KERNEL_REPO="https://github.com/pascua28/kernel_samsung_r8q/releases/latest/download"
 
-cp -a "$MODPATH/boot.img" "$WORK_DIR/kernel"
-cp -a "$MODPATH/dtbo.img" "$WORK_DIR/kernel"
+LOG "- Downloading Paradigm kernel"
+[ -f "$WORK_DIR/kernel/boot.img" ] && rm -f "$WORK_DIR/kernel/boot.img"
+[ -f "$WORK_DIR/kernel/dtbo.img" ] && rm -f "$WORK_DIR/kernel/dtbo.img"
+
+DOWNLOAD_FILE "$KERNEL_REPO/boot.img" "$WORK_DIR/kernel/boot.img"
+DOWNLOAD_FILE "$KERNEL_REPO/dtbo.img" "$WORK_DIR/kernel/dtbo.img"
